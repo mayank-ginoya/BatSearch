075131da7d02742da8cfb037790ea1f7a6502ea9
Ginoya Mayank Kumar <90211492+mayank-ginoya@users.noreply.github.com>
2022-01-30 12:21:50 +0530

    Update settings.py

--- myfirstproject/settings.py
+++ myfirstproject/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mtfw^j!f+(cbx^@7yi#ugtm7(b!fvj@6^dd!glaz8zgx$4ys3e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = TRUE
 
 ALLOWED_HOSTS = []
 
@@ -122,16 +122,6 @@ WSGI_APPLICATION = 'myfirstproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'BatSearch',
-#         'USER': 'postgres',
-#         'PASSWORD':'Mayank@19',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -177,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tesla.project.demo@gmail.com'
-EMAIL_HOST_PASSWORD = 'ljkqyqszoxvtfagi'
+EMAIL_HOST_USER = 'YOUR_EMAIL_ID'
+EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
